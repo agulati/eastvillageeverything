4e32d9286632561a014e6429228f10b0f2f030a4
Amit Gulati <agulati@tunecore.com>
2014-06-24 19:12:34 -0400

    formatting clean up

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 var app = angular.module('eastVillageEverything', ['ngSanitize']);
 
 app.controller('tagsController', [ '$scope', '$http', function($scope, $http) {
-  $http.get("http://0.0.0.0:3000/api/tags").success( function (tags) {
+  $http.get("http://54.84.219.81:8000/api/tags").success( function (tags) {
     $scope.tags = tags
   })
 }])
 
 app.controller('placesController', [ '$scope', '$http', '$sanitize', function($scope, $http, $sanitize) {
-  $http.get("http://0.0.0.0:3000/api/places").success( function (places) {
+  $http.get("http://54.84.219.81:8000/api/places").success( function (places) {
     $scope.places = _.map(places, function (place) {
       place.displayPhone = formatLocal("US", place.phone)
       place.tags = !!place.tags ? place.tags.join(" ") : ""
+      console.log(place)
       return place
     })
   })
