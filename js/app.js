5d4affa9c11d697e7bdeaa6628925db88872464d
Amit Gulati <amit@moven.com>
2014-06-23 21:13:23 -0400

    incorporated pulling data from server

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('eastVillageEverything', ['ngSanitize']);
+
+app.controller('tagsController', [ '$scope', '$http', function($scope, $http) {
+  $http.get("http://0.0.0.0:3000/api/tags").success( function (tags) {
+    $scope.tags = tags
+  })
+}])
+
+app.controller('placesController', [ '$scope', '$http', '$sanitize', function($scope, $http, $sanitize) {
+  $http.get("http://0.0.0.0:3000/api/places").success( function (places) {
+    $scope.places = _.map(places, function (place) {
+      place.displayPhone = formatLocal("US", place.phone)
+      place.tags = !!place.tags ? place.tags.join(" ") : ""
+      return place
+    })
+  })
+}])
