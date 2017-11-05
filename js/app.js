ab3c6883448afd09d2b12b115e10441b43f50676
Amit Gulati <agulati@handy.com>
2017-11-05 12:37:19 -0500

    Kick it up a notch
    - removed angular
    - replaced bootstrap with foundation
    - added loading indicator
    - gave it a face lift

--- js/app.js
+++ js/app.js
@@ -1,17 +1,77 @@
-var app = angular.module('eastVillageEverything', ['ngSanitize']);
+var EastVillageEverything = (function () {
+  return {
+    init: function () {
+      this.fetchTags()
+      this.fetchPlaces()
 
-app.controller('tagsController', [ '$scope', '$http', function($scope, $http) {
-  $http.get("http://54.84.219.81:8000/api/tags").success( function (tags) {
-    $scope.tags = tags
-  })
-}])
+      $("#tag-list").on("change", this.filterPlaces)
+    },
 
-app.controller('placesController', [ '$scope', '$http', '$sanitize', function($scope, $http, $sanitize) {
-  $http.get("http://54.84.219.81:8000/api/places").success( function (places) {
-    $scope.places = _.map(places, function (place) {
-      place.displayPhone = formatLocal("US", place.phone)
+    getRemoteData: function (dataType, success) {
+      var remoteHost = "http://54.84.219.81:8000/api/"
+      $.get(remoteHost + dataType, success)
+    },
+
+    fetchTags: function () {
+      this.getRemoteData("tags", function (data) {
+        var $dropDown = $("#tag-list")
+
+        $(data).each(function () {
+          $dropDown.append($("<option>", {
+            value: this.value,
+            text:  this.display
+          }))
+        })
+      })
+    },
+
+    formatPlace: function (place) {
+      if (place.phone !== "") {
+        place.displayPhone = "(" + place.phone.substr(0,3) + ") " + place.phone.substr(3,3) + "-" + place.phone.substr(6,4)
+      }
       place.tags = !!place.tags ? place.tags.join(" ") : ""
       return place
-    })
-  })
-}])
+    },
+
+    fetchPlaces: function () {
+      var $places   = $("#places")
+      var source    = $("#place-template").html()
+      var template  = Handlebars.compile(source)
+      var parent    = this
+      this.getRemoteData("places", function (data) {
+        $(data).each(function () {
+          $places.append(template(parent.formatPlace(this)))
+        })
+      })
+    },
+
+    filterPlaces: function () {
+      $("#loading").css("visibility", "visible")
+      $("#places").css("visibility", "hidden")
+
+      setTimeout(function () {
+        var filterValue = $("#tag-list").val()
+        if (filterValue === "none") {
+          $.each($("div.bar"), function () {
+            $(this).show()
+          })
+        } else {
+          $.each($("div.bar").filter("." + filterValue), function () {
+            $(this).show()
+          })
+
+          $.each($("div.bar").not("." + filterValue), function () {
+            $(this).hide()
+          })
+        }
+
+        $("#loading").css("visibility", "hidden")
+        $("#places").css("visibility", "visible")
+      }, 500)
+    }
+  }
+}())
+
+$(document).ready(function () {
+  EastVillageEverything.init()
+})
--- stylus.sh
+++
@@ -1,2 +0,0 @@
-stylus --inline --watch css/*.styl
-
